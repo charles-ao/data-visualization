1b598fece0d0253b2bb8e85cee129a71acb00a41
Adedunmola Adeiye <adeiyecharles@gmail.com>
2021-06-18 21:57:00 +0100

    second commit

--- index.js
+++ index.js
@@ -1,363 +1,277 @@
-// var matrix = [
-//     [1, 2, 3, 4],
-//     [5, 6, 7, 8],
-//     [9, 10, 11, 12],
-//     [13, 14, 15, 16]
-// ];
-
-// let tr = d3.select('body')
-//     .append('table')
-//     .selectAll('tr')
-//     .data(matrix)
-//     .enter()
-//     .append('tr')
-
-// let td = d3.selectAll('tr')
-//     .selectAll('td')
-//     .data(function(d){
-//         console.log(d);
-//         return d
-//     })
-//     .enter()
-//     .append('td')
-//     .text(function(d) {
-//         return d
-//     })
-
-// let svg = d3.select('div').append('svg')
-
-// let bar1 = svg.append('rect').attr('height', 15).attr('width', 50).attr("fill", "blue").attr("x", 50).attr("y", 20)
-
-// let bar2 = svg.append('rect').attr('height', 30).attr('width', 50).attr("fill", "pink").attr("x", 100).attr("y", 20)
-
-// let bar3 = svg.append('rect').attr('height', 20).attr('width', 50).attr("fill", "red").attr("x", 150).attr("y", 20)
-
-
-
-// let data = [{
-//     "name": "Jon",
-//     "age": 30,
-//     "location": "The Wall"
-// },
-// {
-//     "name": "Arya",
-//     "age": 12,
-//     "location": "Braavos"
-// },
-// {
-//     "name": "Cersei",
-//     "age": 42,
-//     "location": "Kings Landing"
-// },
-// {
-//     "name": "Tyrion",
-//     "age": 40,
-//     "location": "Kings Landing "
-// }]
-
-//     let faces = d3.select('div')
-//     .selectAll('p')
-//     .data(data)
-//     .enter()
-//     .append('p')
-//     .text(function(d){
-//         return d.name + ''+ d.age
-//     })
-
-// let width = 1000;
-// let height = 500;
-
-// let data = [10, 15, 20, 25, 30];
-// let colors = ['#ffffcc','#c2e699','#78c679','#31a354','#006837'];
-
-// let svg = d3.select('div')
-//     .append('svg') 
-//     .attr('width', width)
-//     .attr('height', height)
-
-
-// let circle = svg.selectAll('g')
-//     .data(data)
-//     .enter()
-//     .append('g')
-//     .attr('transform', 'translate(20,0)')
- 
-
-// circle.append('circle')
-//     .attr('r', function(d){
-//         return d*2
-//     })
-//     .attr('cx', function(d,i){
-//         return i*150
-//     })
-//     .attr('cy', 100)
-//     .attr('fill', function(d,i){
-//         return colors[i]
-//     })
-
-//     circle.append('text')
-//     .attr('x', function(d,i){
-//         return i*145;
-//     })
-//     .attr('y',100)
-//     .attr('font-size','12px')
-//     .attr('font-family','sans-serif')
-//     .attr('stroke','teal')
-//     .text(function(d){
-//         return d
-//     })
-
-
-// var width = 500
-// var height = 500;
-
-// var data = [150, 200, 300, 100, 250];
-// var color = ['#ffffcc','#c2e699','#78c679','#31a354','#006837']
-
-// var scale = d3.scaleLinear()
-//             .domain([d3.min(data),d3.max(data)])
-//             .range([50,200])
-
-//             var scaled = d3.scaleLinear()
-//             .domain([d3.min(data),d3.max(data)])
-//             .range([0,200])
-
-// var svg = d3.select('div')
-//             .append('svg')
-//             .attr('width', width)
-//             .attr('height', height)
-
-
- 
-
-// var g = svg.selectAll('g')
-//         .data(data)
-//         .enter()
-//         .append('g')
-        
-
-// var rect = g.append('rect')
-//         .attr('height', function(d,i){
-//             return scale(d)
-//         })
-//         .attr('width', scale(100))
-//         .attr('y', (d,i) => {
-//             return height - scale(d) - height/2
-//         })
-//         .attr('x', function(d,i){
-//             return i*scale(100)
-//         })
-//         .attr('class', 'bar')
-//         .attr('fill', (d,i) => {
-//             return color[i]
-//         })
-
-// var xaxis = d3.axisBottom()
-//         .scale(scaled)
-
-// var axe = svg.append('g')
-// .attr('transform', 'translate(0,'+height/2+')')
-
-//         .call(xaxis);
-
-// var width = 400,
-// height = 400;
-
-// var data = [10, 15, 20, 25, 30];
-
-// let svg = d3.select('body')
-//             .select('#my-id')
-//             .append('svg')
-//             .attr('width',width)
-//             .attr('height',height)
-
-// let xScale  = d3.scaleLinear()
-//                 .domain([0,d3.max(data)])
-//                 .range([0,width-100])
-
-// let yScale = d3.scaleLinear()
-//                 .domain([0,d3.max(data)])
-//                 .range([height/2,0])
-
-// let xAxis = d3.axisBottom()
-//               .scale(xScale)
-
-// let yAxis = d3.axisLeft()
-//               .scale(yScale)
-
-// let g1 = svg.append('g')
-//         .attr('transform', 'translate(20,20)')
-//         .call(yAxis)
-
-// let g2 = svg.append('g')
-//         .attr('transform', 'translate(20,220)')
-//         .call(xAxis)
-
-// Data Visualization for Predot Stocks
-
-const data = [
-                [2011, 40],
-                [2012, 50],
-                [2013, 65],
-                [2014, 50],
-                [2015, 75],
-                [2016, 80]
-            ]
-
-let width = 700
+let width = 900
 let height = 500
-let margin = 100
-let h = height - margin
-let w = width - margin
+let padding = 50
+
+const GDP_API_URL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+const COUNTRY_API_URL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+const EDUCATION_API_URL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+const CYCLIST_API_URL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
 
 
-// SVG created
-let svg = d3.select('#bar-chart').append('svg').attr('width',width).attr('height',height)
 
-//General group container for elements
-let g = svg.append('g')
+let barChart = d3.select('body')
+                .select('#bar-chart')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
 
-// Scale for x-axis
-let xScale = d3.scaleBand()
-                .range([0, w])
-                .padding(0.4) 
-                .domain(data.map(function(d) { return d[0]; }))
+let choropleth = d3.select('body')
+                .select('#choropleth')
+                .append('svg')
+                .attr('width', (width+100))
+                .attr('height', (height+100))
 
-// Scale for y-axis
-let yScale = d3.scaleLinear()
-                .range([h,0])
-                .domain([0,d3.max(data, function(d) { return d[1] })])
-
-// Scale for bars
-let scaled = d3.scaleLinear()
-                .range([200,h])
-                .domain([d3.min(data, function(d) { return d[1] }),d3.max(data, function(d) { return d[1] })])
-
-//Added x-axis to chart
-g.append('g')
-    .attr('transform', 'translate('+margin+','+(h+50)+')')
-    .call(d3.axisBottom(xScale))
-    .append('text')
-    .attr('y',margin/3)
-    .attr('dx', w-20)
-    .attr('stroke', 'black')
-    .text('Years')
+let scatterPlot = d3.select('body')
+                .select('#scatterplot')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
     
-//Added y-axis to chart
-g.append('g')
-    .attr('transform', 'translate('+margin+',50)')
-    .call(d3.axisLeft(yScale))
-    .append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('y',-margin/3)
-    .attr('dx', 0)
-    .attr('stroke', 'black')
-    .text('Price of Stock ($)')
 
-//Added chart label to chart
-g.append('text')
-    .attr('x', w/2)
-    .attr('y', 20)
-    .attr('stroke', 'black')
-    .text('Predot Stocks Across Six (6) Years')
+d3.json(GDP_API_URL, function(error, param) {
+    if (error) {
+        return console.warn(error);
+    }
+    const data = param.data
 
+    let xScale = d3.scaleBand()
+                .range([0,width-(2*padding)])
+                .domain((data.map(d => (d[0]).slice(0,4))))
+                .padding(0.1)
 
-//Added bars to chart
-g.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    // .on('mouseover', onMouseOver)
-    // .on('mouseout', onMouseOut)
-    .attr('x', (d,i)=>{ return xScale(d[0])+margin})
-    .attr('y', (d,i)=> { return h - scaled(d[1]) + 50} )
-    .attr('width', xScale.bandwidth())
-    .transition()
-    .ease(d3.easeLinear)
-    .duration(500)
-    .delay(function (d, i) {
-        return i * 40;
-    })
-    .attr('height',(d,i)=> { return scaled(d[1]) })
+    let yScale = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d[1])])       
+                .range([height-(2*padding),0]);
+
+    let grpOne = barChart.append('g')
+                .attr('transform','translate('+padding+','+padding+')')
 
 
 
-// function onMouseOver (d,i) {
-//     d3.select(this).attr('class', 'highlighted');
+    grpOne.append('g')
+        .call(d3.axisLeft(yScale).tickFormat(d=> '$'+d))
+        .attr('id','y-axis')
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', padding/2)
+        .attr('x', padding/10)
+        .attr('stroke', 'black')
+        .style('font-size','16px')
+        .text('Gross Domestic Product')
 
-//     g.append("text")
-//     .attr('class', 'val') 
-//     .attr('x', function(d,i) {
-//         return xScale(d[0]);
-//     })
-//     .attr('y', function() {
-//         return yScale(d[1]);
-//     })
-//     .text(function() {
-//         return [ '$' +d[1]];  // Value of the text
-//     })
-
-// }
-
-// function onMouseOut (d,i) {
-//     d3.select(this).attr('class', 'bar');
-
-// }
-let stat = [6, 4, 8, 2, 10];
-let food = ['Spaghetti', 'Rice', 'Yam', 'Beans', 'Noodles']
-
-
-let sWidth = 500
-let sHeight = 400
-let radius = Math.min(sWidth, sHeight) / 2;
-
-let color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c', '#ff456a']);
-
-
-let svge = d3.select('body')
-            .select('#pie-chart')
-            .append("svg")
-            .attr('width',sWidth+100 )
-            .attr('height', sHeight+100)
-
-          
-let gr = svge.append("g")
-            .attr("transform", "translate(" + sWidth / 1.5 + "," + sHeight / 1.5 + ")");
-
-let pie = d3.pie()
-
-let arc = d3.arc()
-            .outerRadius(radius - 10)
-            .innerRadius(0);
-
-let label = d3.arc()
-            .outerRadius(radius)
-            .innerRadius(radius - 160);
-
-
-let arcs = gr.selectAll(".arc")
-            .data(pie(stat))
-            .enter()
-            .append("g")
-            .attr("class", "arc");
-
-    arcs.append("path")
-        .attr("d", arc)
-        .attr("fill", function(d,i) { return color(i); });
+    grpOne.append('g')
+        .attr('transform', 'translate(0,'+(height-(2*padding))+')')
+        .call(d3.axisBottom(xScale)
+            .tickValues(xScale.domain()
+                    .filter(function(d,i){ return !(i%5)})
+                )
+            )
+        .attr('id','x-axis')
+        .append('text')
+        .attr('x',400)
+        .attr('y', 40)
+        .attr('stroke', 'black')
+        .style('font-size','16px')
+        .text('Years')
         
-        
-    arcs.append("text")
-        .attr("transform", function(d) { 
-                 return "translate(" + label.centroid(d) + ")"; 
-         })
-        .style('font-size', '12px')
-        .style('font-weight', 'bold')
-        .text(function(d,i) { return food[i] })
-      
+    grpOne.selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('rect') 
+        .attr('class', 'bar')
+        .attr('data-date',d => d[0])
+        .attr('data-gdp',d => d[1])
+        .attr("x", d => xScale((d[0]).slice(0,4)))
+        .attr("y", d => yScale(d[1]))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d =>  (height-(2*padding)) - yScale(d[1]))
+
+        .append('title')
+        .attr('id','tooltip')
+        .attr('data-date', d => d[0])
+        .text(d => 'Year: ' +d[0]+ ', GDP: ' + d[1] )
+
+    
+});
+
+d3.json(COUNTRY_API_URL, function(error, data){
+    if(error) {
+        console.log(error);
+    } else {
+        let color = ['#55a8ed','#5199d3','#3d74a0','#335e82']
+        let colorText = ['< 15%','15% - 30%','30% - 45%','> 45%']
+        let country = topojson.feature(data, data.objects.counties).features
+
+        console.log(country);
+
+        d3.json(EDUCATION_API_URL,function(error,data){
+            if(error) {
+                console.log(error);
+            } else {
+                let education = data
+                console.log(education);
+                
+                choropleth.selectAll('path')
+                    .data(country)
+                    .enter()
+                    .append('path')
+                    .attr('d', d3.geoPath())
+                    .attr('class', 'pattern')
+                    .attr('fill', (d,i) =>{
+                        let id = d['id']
+                        let county = education.find(item=> item['fips'] === id)
+                        let percentage = county['bachelorsOrHigher']
             
+                        if(percentage <= 15) {
+                            return color[0]
+                        } else if(percentage <= 30) {
+                            return color[1]
+                        } else if (percentage <= 45) {
+                            return color[2]
+                        } else {
+                            return color[3]
+                        }
+                    })
 
-    svge.append("g")
-        .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
-        .append("text")
-        .text("Food Consumption for April 2021")
-        .attr("class", "title")
+                    .append('title')
+                    .text((d)=>{
+            
+                        let id = d['id'];
+                        let county = education.find((item) => item['fips'] === id)
+            
+                        return (county['fips'] + ' - ' + county['area_name'] + ', ' +
+                        county['state'] + ' : ' + county['bachelorsOrHigher'] + '%')
+            
+                    })
+
+                choropleth.selectAll('g')
+                    .data(color)
+                    .enter()
+                    .append('g')
+                    .append('rect')
+                    .attr('x', 750)
+                    .attr('y', (d,i)=>i*20)
+                    .attr('width', 20)
+                    .attr('height', 20)
+                    .attr('fill', d=>d)
+
+                choropleth.selectAll('g')
+                    .data(colorText)
+                    .append('text')
+                    .attr('x', 780)
+                    .attr('y', (d,i)=>(i*20)+15)
+                    .style('font-size','12px')
+                    .text(d=>d)
+            }
+        })
+
+    }
+
+
+})
+
+d3.json(CYCLIST_API_URL, function(error, data) {
+    if(error) {
+        console.log(error);
+    } else {
+
+        let years = data.map(val => val['Year'])
+        let seconds = data.map(val => val['Seconds'])
+        
+        let xScale =  d3.scaleLinear()
+                        .domain([d3.min(years), d3.max(years)])
+                        .range([0,width-(2*padding)])
+
+        let yScale = d3.scaleLinear()
+                        .domain([d3.min(seconds), d3.max(seconds)])
+                        .range([height-(2*padding),0])
+              
+
+        let grpTwo = scatterPlot.append('g')
+                        .attr('transform','translate('+(padding+20)+','+padding+')')
+
+        grpTwo.append('g')
+            .call(d3.axisLeft(yScale)
+            .tickFormat(function(d){
+                    let min = Math.floor(d/60);
+                    let sec = d%60;
+            
+                    return min+":"+sec
+                })
+            )
+            .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', -45)
+            .attr('x', -100)
+            .attr('stroke', 'black')
+            .style('font-size','14px')
+            .text('Time in Minutes')    
+
+        grpTwo.append('g')
+            .attr('transform', 'translate(0,'+(height-(2*padding))+')')
+            .call(d3.axisBottom(xScale)
+                .tickFormat(d => d))
+            .append('text')
+            .attr('y', 40)
+            .attr('x', 400)
+            .attr('stroke', 'black')
+            .style('font-size','14px')
+            .text('Years') 
+
+        grpTwo.selectAll('circle')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('class', 'pattern')
+
+            .attr('r', 7)
+            .attr('cx', (d,i) => xScale(d['Year']))
+            .attr('cy', (d,i) => yScale(d['Seconds']))
+            .attr('fill', (d,i) => {
+                if(d['Doping'] === '') {
+                    return 'seagreen'
+                } else {
+                    return 'steelblue'
+                }
+            })
+
+            .append('title')
+            .text((d,i) => {
+                return `
+                Name: ${d['Name']}
+                Year: ${d['Year']}, Time: ${d['Time']}
+                ${d['Doping']}
+                `
+            })
+        
+        let legend = grpTwo.append('g')
+
+        legend.append('rect')
+            .attr('transform', 'translate('+(width-(5.5*padding))+','+(height-(4*padding))+')')
+            .attr('width',18)
+            .attr('height',18)
+            .attr('fill', 'steelblue') 
+
+        legend.append('rect')
+            .attr('transform', 'translate('+(width-(5.5*padding))+','+(height-(3.5*padding))+')')
+            .attr('width',18)
+            .attr('height',18)
+            .attr('fill', 'seagreen') 
+
+        legend.append('text')
+            .attr('x', width-(5*padding))
+            .attr('y', height-(3.7*padding))
+            .attr('stroke', 'black')
+            .style('font-size','12px')
+            .text('Riders with Doping Allegations')
+
+        
+        legend.append('text')
+            .attr('x', width-(5*padding))
+            .attr('y', height-(3.2*padding))
+            .attr('stroke', 'black')
+            .style('font-size','12px')
+            .text('No Doping Allegations')
+    }
+
+})
